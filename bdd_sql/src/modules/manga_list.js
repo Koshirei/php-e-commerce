2dfc60237922f6d2a4c91894d5b84782b3bc2944
Koshirei <axelaxelc@aol.fr>
2022-12-14 22:32:00 +0100

    ajout mangas

--- bdd_sql/src/modules/manga_list.js
+++ bdd_sql/src/modules/manga_list.js
@@ -10,8 +10,20 @@ export const mangas = [
         cover: "https://okdiario.com/guiltybit/wp-content/uploads/2022/07/manga-one-piece-1055-spoilers.webp"
     }, 
     {
-        name:"bluelock",
+        name:"BlueLock",
         category: "Sport",
-        cover: "https://cdn-www.konbini.com/files/2022/11/Une-Blue-Lock.png"
+        cover: "https://cdn-www.konbini.com/files/2022/11/Une-Blue-Lock.png",
+    },{
+        name:"Love is War",
+        category: "Romance",
+        cover: "https://www.ledojomanga.com/bdd_local/upload/images/Kaguya-sama-Love-Is-War-visuel-2.jpg"
+    },{
+        name:"One Punch Man",
+        category: "Hero",
+        cover: "https://i.pinimg.com/originals/04/f1/f7/04f1f784c2db5b1ee03f1481da35c548.jpg"
+    },{
+        name: "God of Highschool",
+        category: "Shonen",
+        cover: "https://www.myutaku.com/media/anime/poster/346951.jpg"
     }
 ]
